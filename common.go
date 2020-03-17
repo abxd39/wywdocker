598b2c3bce636333f1934c9aa945e9820cc1c429
abxd39 <764137119@qq.com>
2020-03-17 10:32:39 +0800

    add wywfunc

---
+++ common.go
@@ -0,0 +1,15 @@
+/**
+ * @Author: wangyingwen
+ * @Description: 
+ * @File:  common
+ * @Version: 1.0.0
+ * @Date: 2020-03-17 10:30
+ */
+
+package main
+
+import "errors"
+
+func WywBranch(s string)error{
+	return errors.New(s)
+}
